9729003e4f35eb27a0ec161631956bd32581d169
longzili <longzilipro@gmail.com>
2024-02-03 10:53:43 +0800

    <update> temp core fast

---
+++ RUN.sh
@@ -0,0 +1,7 @@
+
+./run_batch.sh \
+"source" \
+"target" \
+"output" \
+
+
---
+++ run_fast.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from facefusion import core_fast
+
+if __name__ == '__main__':
+    core_fast.cli()
